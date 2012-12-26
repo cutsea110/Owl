f6ae574eafc4f19f8f4d75f93be24eaeebd79305
cutsea110 <cutsea110@gmail.com>
2012-12-26 18:26:00 +0900

    Add page-header for homepage

--- Handler/Home.hs
+++ Handler/Home.hs
@@ -2,13 +2,15 @@
 module Handler.Home where
 
 import Import
+import Yesod.Auth
 
 getHomeR :: Handler RepHtml
 getHomeR = do
-    defaultLayout $ do
-        let accountId = $(widgetFile "account-id")
-            password = $(widgetFile "password")
-            email = $(widgetFile "email")
-            profile = $(widgetFile "profile")
-        setTitle "Home"
-        $(widgetFile "homepage")
+  u <- requireAuth
+  defaultLayout $ do
+    let accountId = $(widgetFile "account-id")
+        password = $(widgetFile "password")
+        email = $(widgetFile "email")
+        profile = $(widgetFile "profile")
+    setTitle "Home"
+    $(widgetFile "homepage")
