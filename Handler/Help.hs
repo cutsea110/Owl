dd1d110aac6d01a099c20ef69a95e513e6270f68
cutsea110 <cutsea110@gmail.com>
2012-12-27 09:43:34 +0900

    Add pages password-reset and usage on help

--- Handler/Help.hs
+++ Handler/Help.hs
@@ -5,5 +5,7 @@ import Import
 getHelpR :: Handler RepHtml
 getHelpR = do
   defaultLayout $ do
+    let passreset = $(widgetFile "password-reset")
+        usage = $(widgetFile "usage")
     setTitle "Help"
     $(widgetFile "help")
