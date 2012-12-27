e9e5e5c9c42954f7b7c78eb2a8f27ecc01898b2d
cutsea110 <cutsea110@gmail.com>
2012-12-27 22:28:51 +0900

    Update design

--- Handler/AdminTools.hs
+++ Handler/AdminTools.hs
@@ -4,15 +4,15 @@ import Import
 
 getAdminToolsR :: Handler RepHtml
 getAdminToolsR = do
-  let photos = [ (img_avatar_avatar_jpg, "Photo 1"::Text)
-               , (img_avatar_avatar2_jpg, "Photo 2")
-               , (img_avatar_avatar3_jpg, "Photo 3")
-               , (img_avatar_avatar4_jpg, "Photo 4")
-               , (img_avatar_avatar5_jpg, "Photo 5")
-               , (img_avatar_avatar6_jpg, "Photo 6")
-               , (img_avatar_avatar7_jpg, "Photo 7")
-               , (img_avatar_avatar8_jpg, "Photo 8")
-               , (img_avatar_avatar9_jpg, "Photo 9")
+  let photos = [ (img_avatar_avatar_jpg, "User 1"::Text)
+               , (img_avatar_avatar2_jpg, "User 2")
+               , (img_avatar_avatar3_jpg, "User 3")
+               , (img_avatar_avatar4_jpg, "User 4")
+               , (img_avatar_avatar5_jpg, "User 5")
+               , (img_avatar_avatar6_jpg, "User 6")
+               , (img_avatar_avatar7_jpg, "User 7")
+               , (img_avatar_avatar8_jpg, "User 8")
+               , (img_avatar_avatar9_jpg, "User 9")
                ]
   defaultLayout $ do
     let userlist = $(widgetFile "user-list")
