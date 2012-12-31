4c5853637925f091ded78ac50bb8c13aa9a9529b
cutsea110 <cutsea110@gmail.com>
2012-12-31 10:43:13 +0900

    Make password-reset as bootstrap like widget by using newIdent and renderBootstrap

--- Handler/Help.hs
+++ Handler/Help.hs
@@ -1,11 +1,31 @@
-module Handler.Help where
+module Handler.Help 
+       ( getHelpR
+       ) where
 
 import Import
+import Text.Julius (rawJS)
+import Data.Text (Text)
 
 getHelpR :: Handler RepHtml
 getHelpR = do
   defaultLayout $ do
-    let passreset = $(widgetFile "password-reset")
-        usage = $(widgetFile "usage")
+    let usage = $(widgetFile "usage")
     setTitle "Help"
     $(widgetFile "help")
+
+passreset :: Widget
+passreset = do
+  ident <- lift newIdent
+  (w, e) <- lift $ generateFormPost $ renderBootstrap $ ef ident
+  $(widgetFile "password-reset")
+
+ef :: (RenderMessage master AppMessage, RenderMessage master FormMessage) =>
+      Text -> AForm sub master Text
+ef x = areq emailField fs Nothing
+  where
+    fs = FieldSettings { fsLabel = SomeMessage MsgEmail
+                       , fsTooltip = Nothing
+                       , fsId = Just x
+                       , fsName = Nothing
+                       , fsAttrs = [("class", "span3")]
+                       }
