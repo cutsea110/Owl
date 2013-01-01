5134a26d86cba0e3137e64591097bd1ccdd44d34
cutsea110 <cutsea110@gmail.com>
2013-01-01 10:47:15 +0900

    Add PasswordR handler

--- Handler/Home.hs
+++ Handler/Home.hs
@@ -6,6 +6,7 @@ import Prelude (head, tail)
 import Yesod.Auth
 import Data.Maybe (isJust)
 import qualified Data.Text as T
+import Data.Tuple.HT (fst3, snd3, thd3)
 
 accountForm :: Maybe Text -> Html -> MForm App App (FormResult Text, Widget)
 accountForm mv fragment = do
@@ -30,12 +31,63 @@ accountForm mv fragment = do
                        , fsAttrs = [("class", "span2")]
                        }
 
+passwordForm :: Maybe (Text,Text,Text) -> Html -> MForm App App (FormResult Text, Widget)
+passwordForm mv fragment = do
+  (res0, view0) <- mreq passwordField (fs MsgCurrentPassword) (fst3 <$> mv)
+  (res1, view1) <- mreq passwordField (fs MsgNewPassword) (snd3 <$> mv)
+  (res2, view2) <- mreq passwordField (fs MsgConfirmPassword) (thd3 <$> mv)
+  let res = case (res0, res1, res2) of
+        (FormSuccess x, FormSuccess y, FormSuccess z) ->
+          if y == z
+          then FormSuccess y
+          else FormFailure ["don't match between new password and confirmation"]
+        _ -> FormFailure ["fail to password update!!"]
+  liftIO $ putStrLn $ show (res0, res1, res2)
+  let widget = [whamlet|
+\#{fragment}
+<div .control-group.info .clearfix :fvRequired view0:.required :not $ fvRequired view0:.optional :isJust $ fvErrors view0:.error>
+  <label .control-label for=#{fvId view0}>#{fvLabel view0}
+  <div .controls .input>
+    ^{fvInput view0}
+    $maybe tt <- fvTooltip view0
+      <span .help-block>#{tt}
+    $maybe err <- fvErrors view0
+      <span .help-block>#{err}
+
+<div .control-group.warning .clearfix :fvRequired view1:.required :not $ fvRequired view1:.optional :isJust $ fvErrors view1:.error>
+  <label .control-label for=#{fvId view1}>#{fvLabel view1}
+  <div .controls .input>
+    ^{fvInput view1}
+    $maybe tt <- fvTooltip view1
+      <span .help-block>#{tt}
+    $maybe err <- fvErrors view1
+      <span .help-block>#{err}
+
+<div .control-group.warning .clearfix :fvRequired view2:.required :not $ fvRequired view2:.optional :isJust $ fvErrors view2:.error>
+  <label .control-label for=#{fvId view2}>#{fvLabel view2}
+  <div .controls .input>
+    ^{fvInput view2}
+    $maybe tt <- fvTooltip view2
+      <span .help-block>#{tt}
+    $maybe err <- fvErrors view2
+      <span .help-block>#{err}
+|]
+  return (res, widget)
+  where
+    fs l = FieldSettings { fsLabel = SomeMessage l
+                         , fsTooltip = Nothing
+                         , fsId = Nothing
+                         , fsName = Nothing
+                         , fsAttrs = []
+                         }
+
 getHomeR :: Handler RepHtml
 getHomeR = do
   u <- requireAuth
   (menu1, menu2, menu3, menu4) <- (,,,) <$> newIdent <*> newIdent <*> newIdent <*> newIdent
   (modal1, modal2, modal3) <- (,,) <$> newIdent <*> newIdent <*> newIdent
-  (w, e) <- generateFormPost $ accountForm Nothing  
+  (w, e) <- generateFormPost $ accountForm Nothing
+  (wp, ep) <- generateFormPost $ passwordForm Nothing
   tabIs <- fmap (maybe ("account-id"==) (==)) $ lookupGetParam "tab"
   mmsg <- getMessage
   let photos = [ (img_avatar_avatar_jpg, "Photo 1"::Text)
@@ -69,3 +121,15 @@ postAccountIdR = do
       setMessage "Update account..."
       redirect ((HOME HomeR), [("tab", "account-id")])
     _ -> redirect ((HOME HomeR), [("tab", "account-id")])
+
+postPasswordR :: Handler ()
+postPasswordR = do
+  ((r, _), _) <- runFormPost $ passwordForm Nothing
+  case r of
+    FormSuccess x -> do
+      liftIO $ putStrLn $ "[TODO] update password " ++ T.unpack x
+      setMessage "Update password..."
+      redirect ((HOME HomeR), [("tab", "password")])
+    FormFailure (x:_) -> do
+      setMessage x
+      redirect ((HOME HomeR), [("tab", "password")])
