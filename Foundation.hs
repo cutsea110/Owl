293dbcc5fe9f9e9080ef972b3b53f8df62a481e5
Katsutoshi Itoh <cusea110@gmail.com>
2013-01-08 09:49:30 +0900

    Replace jquery's CDN by googleapis, because https
    When owl hosted on https, the original code's CDN(http) coouldn't loaded.

--- Foundation.hs
+++ Foundation.hs
@@ -81,7 +81,7 @@ instance Yesod App where
         -- you to use normal widget features in default-layout.
 
         pc <- widgetToPageContent $ do
-          addScriptRemote "http://code.jquery.com/jquery-1.8.0.min.js"
+          addScriptRemote "https://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"
           addStylesheet $ StaticR css_bootstrap_min_css
 --          addStylesheet $ StaticR css_bootstrap_responsive_min_css
           addScript $ StaticR js_bootstrap_min_js
