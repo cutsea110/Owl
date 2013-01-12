096cede6c0a76a8a9be65304a15c6874105455c1
cutsea110 <cutsea110@gmail.com>
2013-01-12 17:47:07 +0900

    Add Verify Status column to email table

--- Model.hs
+++ Model.hs
@@ -5,6 +5,9 @@ import Yesod
 import Data.Text (Text)
 import Database.Persist.Quasi
 
+data VerStatus = Unverified | Verified
+            deriving (Read, Show, Eq, Ord, Enum, Bounded)
+derivePersistField "VerStatus"
 
 -- You can define all of your database entities in the entities file.
 -- You can find more information on persistent and how to declare entities
