3918f7df7bcfba2c5c61f28ee4cdf6e806f5e7f7
cutsea110 <cutsea110@gmail.com>
2013-02-04 17:24:57 +0900

    Mock service

--- Handler/Service.hs
+++ Handler/Service.hs
@@ -3,6 +3,15 @@ module Handler.Service
        ) where
 
 import Import
+import Data.Maybe (fromJust)
 
 postAuthenticateR :: Handler RepJson
-postAuthenticateR = undefined
+postAuthenticateR = do
+  mk <- lookupPostParam "auth"
+  liftIO $ putStrLn $ show mk
+  let (passP, uname) = (maybe False (const True)  mk, mk)
+  jsonToRepJson $ object [ "authenticate" .= passP
+                         , "user" .= object [ "ident" .= uname
+                                            , "email" .= ("cutsea110@gmail.com"::Text)
+                                            ]
+                         ]
