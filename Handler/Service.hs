ae3c8fe23f06f9175b35dce47b67c005b1cc6763
cutsea110 <cutsea110@gmail.com>
2013-02-05 10:40:59 +0900

    Update authentication logic

--- Handler/Service.hs
+++ Handler/Service.hs
@@ -4,14 +4,27 @@ module Handler.Service
 
 import Import
 import Data.Maybe (fromJust)
+import Owl.Helpers.Auth.HashDB (validateUser)
 
 postAuthenticateR :: Handler RepJson
 postAuthenticateR = do
-  mk <- lookupPostParam "auth"
-  liftIO $ putStrLn $ show mk
-  let (passP, uname) = (maybe False (const True)  mk, mk)
-  jsonToRepJson $ object [ "authenticate" .= passP
-                         , "user" .= object [ "ident" .= uname
-                                            , "email" .= ("cutsea110@gmail.com"::Text)
-                                            ]
-                         ]
+  mauth <- (,) <$> lookupPostParam "ident" <*> lookupPostParam "pass"
+  case mauth of
+    (Just ident, Just pass) -> do
+      checked <- validateUser (UniqueUser ident) pass
+      if checked then authenticated ident else don'tAuthenticated (ident, pass)
+    _ -> invalidArgs ["Not found required query parameter."]
+  where
+    authenticated ident = do
+      u <- runDB $ getBy404 (UniqueUser ident)
+      jsonToRepJson $ object [ "authenticate" .= True
+                             , "user" .= object [ "ident" .= ident
+                                                , "email" .= userEmail (entityVal u)
+                                                ]
+                             ]
+    don'tAuthenticated (ident, pass) = do 
+      jsonToRepJson $ object [ "authenticate" .= False
+                             , "rejected" .= object [ "ident" .= ident
+                                                    , "pass" .= pass
+                                                    ]
+                             ]
