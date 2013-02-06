dc9dfda4ca500baf6a01fbbbe3b2b81e21b46265
cutsea110 <cutsea110@gmail.com>
2013-02-07 07:49:14 +0900

    Update api by using response body json style instead of query params

--- Handler/Service.hs
+++ Handler/Service.hs
@@ -2,29 +2,69 @@ module Handler.Service
        ( postAuthenticateR
        ) where
 
-import Import
+import Import hiding (object)
 import Data.Maybe (fromJust)
 import Owl.Helpers.Auth.HashDB (validateUser)
 
+import Control.Monad (mzero, (=<<), (<=<), (>=>))
+import Data.Conduit as C
+import Network.Wai
+import Data.Aeson
+import Data.Aeson.Parser (json)
+import Data.Conduit.Attoparsec (sinkParser)
+import qualified Data.ByteString.Lazy.Char8 as LB
+import qualified Data.ByteString.Lazy.UTF8 as LB
+import Data.Text (Text)
+import qualified Data.Text as T
+import Data.HashMap.Strict as M (toList)
+
+
+-- for Request
+data AuthReq = AuthReq
+               { ident :: Text
+               , pass :: Text
+               }
+             deriving (Show, Read, Eq)
+
+instance FromJSON AuthReq where
+  parseJSON (Object v) = AuthReq <$> v .: "ident" <*> v .: "pass"
+  parseJSON _ = mzero
+instance ToJSON AuthReq where
+  toJSON (AuthReq i p) = object ["ident" .= i, "pass" .= p]
+
+-- for Response
+data AuthRes = Rejected { rejected_ident :: Text, rejected_pass :: Text }
+             | Accepted { accepted_ident :: Text, accepted_email :: Maybe Text }
+             deriving (Show, Read, Eq)
+
+instance FromJSON AuthRes where
+  parseJSON (Object o) = case M.toList o of
+    [("rejected", Object o')] -> Rejected <$> o' .: "ident" <*> o' .: "pass"
+    [("accepted", Object o')] -> Accepted <$> o' .: "ident" <*> o' .:? "email"
+  parseJSON _ = mzero
+
+instance ToJSON AuthRes where
+  toJSON (Rejected i p) = object [ "rejected" .= object [ "ident" .= i
+                                                        , "pass" .= p
+                                                        ]
+                                 ]
+  toJSON (Accepted i me) = object [ "accepted" .= object [ "ident" .= i
+                                                         , "email" .= me
+                                                         ]
+                                  ]
+
 postAuthenticateR :: Handler RepJson
 postAuthenticateR = do
-  mauth <- (,) <$> lookupPostParam "ident" <*> lookupPostParam "pass"
-  case mauth of
-    (Just ident, Just pass) -> do
+  r <- fmap reqWaiRequest getRequest
+  v <- liftIO $ runResourceT $ requestBody r $$ sinkParser json
+  case fromJSON v of
+    Success (AuthReq ident pass) -> do
       checked <- validateUser (UniqueUser ident) pass
       if checked then authenticated ident else don'tAuthenticated (ident, pass)
-    _ -> invalidArgs ["Not found required query parameter."]
+    Error msg -> invalidArgs [T.pack msg]
   where
     authenticated ident = do
       u <- runDB $ getBy404 (UniqueUser ident)
-      jsonToRepJson $ object [ "authenticate" .= True
-                             , "user" .= object [ "ident" .= ident
-                                                , "email" .= userEmail (entityVal u)
-                                                ]
-                             ]
-    don'tAuthenticated (ident, pass) = do 
-      jsonToRepJson $ object [ "authenticate" .= False
-                             , "rejected" .= object [ "ident" .= ident
-                                                    , "pass" .= pass
-                                                    ]
-                             ]
+      jsonToRepJson $ Accepted ident (userEmail (entityVal u))
+    don'tAuthenticated (ident, pass) = do
+      jsonToRepJson $ Rejected ident pass
