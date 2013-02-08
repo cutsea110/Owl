cfabd5a4b1c2cf5c56ba7d719de9ca4e9d6f9a43
cutsea110 <cutsea110@gmail.com>
2013-02-08 13:48:30 +0900

    Add utility functions for RSA

--- Owl/Helpers/Util.hs
+++ Owl/Helpers/Util.hs
@@ -5,17 +5,28 @@ module Owl.Helpers.Util
        , getCurrentRoute'
        , toGravatarHash
        , gravatarUrl
+         -- RSA
+       , genKey
+       , encrypt
+       , decrypt
+       , sign
+       , verify
        ) where
 
-import Prelude (($), (.), return, fmap, Maybe, show, Int, dropWhile, reverse, map)
+import Prelude
 import Yesod
+import qualified Codec.Crypto.RSA as RSA
 import Control.Applicative ((<$>),(<*>))
+import Control.Arrow (first)
+import Crypto.Random
+import qualified Data.ByteString.Char8 as BS
+import qualified Data.ByteString.Base64 as Base64
+import qualified Data.ByteString.Lazy.Char8 as BL
 import qualified Data.ByteString.Lazy.UTF8 as BL
 import Data.Char (toLower, isSpace)
 import Data.Digest.Pure.MD5 (md5)
 import Data.Text (Text)
 import qualified Data.Text as T
-
 
 newIdent2 :: Yesod m => GHandler s m (Text, Text)
 newIdent2 = (,) <$> newIdent <*> newIdent
@@ -44,3 +55,36 @@ gravatarUrl s h = T.concat [ "https://secure.gravatar.com/avatar/"
                            , "?d=identicon&s="
                            , T.pack $ show s
                            ]
+
+-- |
+--
+-- RSA utility
+--
+fromLazy :: BL.ByteString -> BS.ByteString
+fromLazy = BS.pack . BL.unpack
+toLazy :: BS.ByteString -> BL.ByteString
+toLazy = BL.pack . BS.unpack
+
+genKey :: IO (RSA.PublicKey, RSA.PrivateKey)
+genKey = (newGenIO::IO SystemRandom) >>= return . fs . flip RSA.generateKeyPair 1024
+  where
+    fs (f, s, _) = (f, s)
+
+encode :: BL.ByteString -> BL.ByteString
+encode = toLazy . Base64.encode . fromLazy
+decode :: BL.ByteString -> BL.ByteString
+decode = either BL.pack toLazy . Base64.decode . fromLazy
+
+encrypt :: RSA.PublicKey -> BL.ByteString -> IO (BL.ByteString, SystemRandom)
+encrypt pub plain = do
+  g <- newGenIO :: IO SystemRandom
+  return $ first encode $ RSA.encrypt g pub plain
+
+decrypt :: RSA.PrivateKey -> BL.ByteString -> BL.ByteString
+decrypt priv cipher = either BL.pack (RSA.decrypt priv . toLazy) $ Base64.decode $ fromLazy cipher
+
+sign :: RSA.PrivateKey -> BL.ByteString -> BL.ByteString
+sign = (encode.).RSA.sign
+
+verify :: RSA.PublicKey -> BL.ByteString -> BL.ByteString -> Bool
+verify pub plain cipher = RSA.verify pub plain $ decode cipher
