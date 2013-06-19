352158de933727aae0c949ef12f71bf22b337b33
cutsea110 <cutsea110@gmail.com>
2013-06-19 10:16:55 +0900

    Update genkey tool for yesod-platform-1.2 system

--- tools/genkey.hs
+++ tools/genkey.hs
@@ -1,15 +1,9 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Main where
 
-import Control.Arrow (first)
-import Crypto.Random
-import qualified Codec.Crypto.RSA as RSA
+import Prelude
 import qualified Crypto.PubKey.OpenSsh as SSH
 import qualified Data.ByteString.Char8 as BS
-import qualified Data.ByteString.UTF8 as BS
-import qualified Data.ByteString.Lazy.Char8 as BL
-import qualified Data.ByteString.Lazy.UTF8 as BL
-import qualified Data.ByteString.Base64 as Base64
 import System.Environment (getArgs)
 import Yesod.Auth.Owl.Util
 
@@ -18,7 +12,7 @@ main = do
   args <- getArgs
   (pub,priv) <- genKey
   let email = if null args then "your@example.com" else head args
-  let epk = SSH.encode (SSH.OpenSshPublicKeyRsa pub $ BS.pack email)
+  let epk = SSH.encodePublic (SSH.OpenSshPublicKeyRsa pub $ BS.pack email)
   putStr "encoded public-key: "
   BS.putStrLn epk
   
